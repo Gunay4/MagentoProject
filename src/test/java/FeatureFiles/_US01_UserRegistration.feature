a584751c563f35d8f8619a5bc86ab06d6e048114
Gulumser Wilson <wilsongulumser@gmail.com>
2024-10-27 15:51:17 -0600

    US01 Registration tamamlandi

---
+++ src/test/java/FeatureFiles/_US01_UserRegistration.feature
@@ -0,0 +1,17 @@
+Feature: User Registration Functionality
+
+  Background:
+    Given Navigate to magento website
+
+    And User navigates to Create Account page
+
+  Scenario: User Registration with Unique Data Entries
+
+    And User enters registration details
+      | firstName | lastName | emailAddress                  | registrationPassword | passwordConfirmation |
+      | Johnyy    | Does     | john.unique885726@example.com | Passw0rd1            | Passw0rd1            |
+      | Janese    | Smithhie | jane.unique986545@example.com | Pass1234             | Pass1234             |
+      | Alexy     | Browniee | alex.unique458448@example.com | Password12           | Password12           |
+
+# Test için mail adreslerini değistirin lütfen
+
--- src/test/java/StepDefinitions/_US01_UserRegistration.java
+++ src/test/java/StepDefinitions/_US01_UserRegistration.java
@@ -1,4 +1,79 @@
 package StepDefinitions;
 
+import Pages.DialogContent;
+import Utilities.GWD;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+
 public class _US01_UserRegistration {
+
+    DialogContent dc = new DialogContent();
+    Actions actions = new Actions(GWD.getDriver());
+    JavascriptExecutor js = (JavascriptExecutor) GWD.getDriver();
+    WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(10));
+
+    @And("User navigates to Create Account page")
+    public void userNavigatesToCreateAccountPage() {
+        wait.until(ExpectedConditions.elementToBeClickable(dc.createAccount));
+        js.executeScript("arguments[0].scrollIntoView(true);", dc.createAccount); // Kaydırma işlemi
+        js.executeScript("arguments[0].click();", dc.createAccount); // Butona tıklama
+    }
+
+    @And("User enters registration details")
+    public void userEntersRegistrationDetails(DataTable dataTable) {
+        List<Map<String, String>> registrationData = dataTable.asMaps(String.class, String.class);
+        System.out.println(registrationData.size());
+
+        for (Map<String, String> data : registrationData) {
+
+            wait.until(ExpectedConditions.visibilityOf(dc.firstName));
+            dc.mySendKeys(dc.firstName, data.get("firstName"));
+
+            wait.until(ExpectedConditions.visibilityOf(dc.lastName));
+            dc.mySendKeys(dc.lastName, data.get("lastName"));
+
+            wait.until(ExpectedConditions.visibilityOf(dc.emailAddress));
+            dc.mySendKeys(dc.emailAddress, data.get("emailAddress"));
+
+            wait.until(ExpectedConditions.visibilityOf(dc.registrationPassword));
+            dc.mySendKeys(dc.registrationPassword, data.get("registrationPassword"));
+
+            wait.until(ExpectedConditions.visibilityOf(dc.passwordConfirmation));
+            dc.mySendKeys(dc.passwordConfirmation, data.get("passwordConfirmation"));
+
+
+            userClicksOnRegisterButton();
+            wait.until(ExpectedConditions.visibilityOf(dc.messageText));
+            Assert.assertTrue(dc.messageText.getText().toLowerCase().contains("thank you for registering"));
+            dc.myClick(dc.customerMenuDropDown);
+            dc.myClick(dc.signOut);
+            dc.myClick(dc.createAccount);
+
+
+        }
+
+        dc.myClick(dc.signIn);
+
+
+    }
+
+    public void userClicksOnRegisterButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(dc.registerButton));
+        js.executeScript("arguments[0].scrollIntoView({block: 'center'});", dc.registerButton);
+        js.executeScript("arguments[0].click();", dc.registerButton);
+    }
+
 }
+
+
+
