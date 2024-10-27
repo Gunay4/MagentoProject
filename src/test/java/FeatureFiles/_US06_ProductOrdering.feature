bfe4c8d1fc6b515c51f8b4d982f29429e4240712
kardelenssu <kurtkardelensu@gmail.com>
2024-10-27 16:04:04 +0100

    US06 completed

---
+++ src/test/java/FeatureFiles/_US06_ProductOrdering.feature
@@ -0,0 +1,34 @@
+Feature: Product Order Functionality
+
+  Background:
+    Given Navigate to magento website
+    And Click on the Element in Dialog
+      | signIn |
+    And User send keys in Dialog
+      | email    | test34@gmail.com |
+      | password | As123456         |
+    And Click on the Element in Dialog
+      | signInButton |
+    Then Login Success Message Should Appear
+
+  Scenario: Order Verification
+
+    And Click on the Element in Dialog for addToCart
+
+    And Shopping transaction in Dialog
+
+    And Click on the Element in Dialog
+      | shippingMethods |
+      | nextButton      |
+
+    Then Purchase Success Message Should Appear
+
+
+
+
+
+
+
+
+
+
--- src/test/java/StepDefinitions/_US06_ProductOrdering.java
+++ src/test/java/StepDefinitions/_US06_ProductOrdering.java
@@ -1,5 +1,48 @@
 package StepDefinitions;
 
-public class _US06_ProductOrdering {
+import Pages.DialogContent;
+import Utilities.GWD;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import java.time.Duration;
 
+
+public class _US06_ProductOrdering {
+    DialogContent dc = new DialogContent();
+    Actions actions = new Actions(GWD.getDriver());
+    JavascriptExecutor js = (JavascriptExecutor) GWD.getDriver();
+    WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(5));
+
+    @And("Click on the Element in Dialog for addToCart")
+    public void clickOnTheElementInDialogForAddToCart() {
+        js.executeScript("arguments[0].scrollIntoView({block: 'center'});", dc.fusionBackpack);
+        actions.moveToElement(dc.fusionBackpack).build().perform();
+        wait.until(ExpectedConditions.elementToBeClickable(dc.addToCart));
+        js.executeScript("arguments[0].click();", dc.addToCart);
+
+    }
+
+    @And("Shopping transaction in Dialog")
+    public void shoppingTransactionInDialog() {
+        js.executeScript("arguments[0].scrollIntoView({block: 'center'});", dc.basket);
+        wait.until(ExpectedConditions.visibilityOf(dc.counterNumber));
+        wait.until(ExpectedConditions.elementToBeClickable(dc.basket));
+        js.executeScript("arguments[0].click();", dc.basket);
+        wait.until(ExpectedConditions.visibilityOf(dc.proceedToCheckout));
+        dc.myClick(dc.proceedToCheckout);
+    }
+
+    @Then("Purchase Success Message Should Appear")
+    public void purchaseSuccessMessageShouldAppear() {
+        wait.until(ExpectedConditions.visibilityOf(dc.placeOrder));
+        js.executeScript("arguments[0].click();", dc.placeOrder);
+        wait.until(ExpectedConditions.urlToBe("https://magento.softwaretestingboard.com/checkout/onepage/success/"));
+        Assert.assertTrue(GWD.getDriver().getCurrentUrl().toLowerCase().contains("success".toLowerCase()));
+
+    }
 }
